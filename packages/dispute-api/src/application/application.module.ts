e93551d371571be612ced73af9662dc6866f2956
Lucas Ferreira <lucas@licitardigital.com.br>
2023-06-16 00:31:16 -0300

    style: improve application module exports

--- packages/dispute-api/src/application/application.module.ts
+++ packages/dispute-api/src/application/application.module.ts
@@ -4,13 +4,16 @@ import { DomainModule } from '@domain/domain.module'
 
 import { AuthV1Api } from './api/auth/auth.v1.api'
 import { UserV1Api } from './api/user/user.v1.api'
-import { UserRepository } from './repository/user.repository'
-import { UserService } from './services/user.service'
+import { UserRepository } from './repository'
+import { UserService } from './services'
+
+const repositories = [UserRepository]
+const services = [UserService]
 
 @Module({
   imports: [forwardRef(() => DomainModule)],
   controllers: [AuthV1Api, UserV1Api],
-  providers: [UserService, UserRepository],
-  exports: [UserService, UserRepository],
+  providers: [...services, ...repositories],
+  exports: [...services, ...repositories],
 })
 export class ApplicationModule {}
