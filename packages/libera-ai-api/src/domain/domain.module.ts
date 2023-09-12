7f91144a6921972aae02ebf8a3b552930d5b84bb
Lucas Ferreira <lucas@licitardigital.com.br>
2023-09-12 20:38:54 -0300

    fix: fix domain module imports

--- packages/libera-ai-api/src/domain/domain.module.ts
+++ packages/libera-ai-api/src/domain/domain.module.ts
@@ -2,13 +2,32 @@ import { forwardRef, Module } from '@nestjs/common'
 
 import { ApplicationModule } from '@application/application.module'
 import { AuthModule } from '@infra/http/auth/auth.module'
-import { AuthenticateUseCase, RegisterUserUseCase } from '@usecases/auth'
+import {
+  AuthenticateUseCase,
+  RegisterUserUseCase,
+  VerifyIndentifierUseCase,
+} from '@usecases/auth'
 import { GetAllUsersUseCase } from '@usecases/user'
 
-import { PasswordValidator, UserValidator } from './validators'
+import {
+  ActivateUserValidator,
+  EmailValidator,
+  PasswordValidator,
+  UserValidator,
+} from './validators'
 
-const useCases = [AuthenticateUseCase, RegisterUserUseCase, GetAllUsersUseCase]
-const validators = [PasswordValidator, UserValidator]
+const useCases = [
+  AuthenticateUseCase,
+  RegisterUserUseCase,
+  VerifyIndentifierUseCase,
+  GetAllUsersUseCase,
+]
+const validators = [
+  ActivateUserValidator,
+  EmailValidator,
+  PasswordValidator,
+  UserValidator,
+]
 
 @Module({
   imports: [forwardRef(() => AuthModule), forwardRef(() => ApplicationModule)],
