2cf931fcb5908931a31407d8b36cebdaaf14fc3a
Lucas Ferreira <lucas@licitardigital.com.br>
2023-09-12 18:38:50 -0300

    feat: add user validators

--- packages/libera-ai-api/src/domain/validators/user.validator.ts
+++ packages/libera-ai-api/src/domain/validators/user.validator.ts
@@ -1,10 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 
+import { UserService } from '@application/services'
+import { Exceptions } from '@core/constants'
 import { User } from '@domain/entities'
+import { DeepPartial } from 'typeorm'
 
 @Injectable()
 export class UserValidator {
+  constructor(
+    @Inject(UserService)
+    private userService: UserService,
+  ) {}
+
   async validate(user: User): Promise<boolean> {
     return !!user
+  }
+
+  async exists(user: DeepPartial<User>): Promise<boolean> {
+    const foundUser = await this.userService.findOneBy([
+      { email: user?.email },
+      { cpf: user?.cpf },
+    ])
+
+    if (!foundUser?.id) throw Exceptions.USER_NOT_FOUND
+
+    return true
   }
 }
